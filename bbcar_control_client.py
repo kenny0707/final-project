a767f3389a667db8052815b51fdca2927a0f9653
kenny0707 <100082366+kenny0707@users.noreply.github.com>
2022-06-01 19:26:34 +0800

    Add files via upload

---
+++ bbcar_control_client.py
@@ -0,0 +1,27 @@
+# led_test_client.py
+# Test client for erpc led server example
+# Author: becksteing/Jing-Jia Liou
+# Date: 02/13/2022
+# Blinks LEDs on a connected Mbed-enabled board running the erpc LED server example
+
+from time import sleep
+import erpc
+from bbcar_control import *
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("Usage: python led_test_client.py <serial port to use>")
+        exit()
+
+    # Initialize all erpc infrastructure
+    xport = erpc.transport.SerialTransport(sys.argv[1], 9600)
+    client_mgr = erpc.client.ClientManager(xport, erpc.basic_codec.BasicCodec)
+    client = client.BBCarServiceClient(client_mgr)
+
+    while True:
+        start = input()
+        if start == "i":
+            client.info()
+        sleep(0.5)
